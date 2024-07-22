bc1cd9d868f233f09995984ee4a3f7048fc0b01c
dennisong45 <dennisongwenghan@gmail.com>
2024-07-21 20:16:59 -0700

    feat: small refactor

--- lexer.go
+++ lexer.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
-    "unicode"
 	"strings"
+	"unicode"
 )
 
 type TokenType int
 
 const (
 	// Literal Types
-	Number TokenType = iota // Iota is Enum = 0
-	Identifier // 1
+	Number     TokenType = iota // Iota is Enum = 0
+	Identifier                  // 1
 	// Reserve VIP Word
 	Let
 	Func
@@ -24,13 +24,14 @@ const (
 	Equals
 	OpenParent
 	CloseParent
+	UnidentifiedFlyingObject = 12
 )
 
 var keywords = map[string]TokenType{
-    "let":  Let,
-    "func": Func,
-	"for": For,
-	"range": Range,
+	"let":        Let,
+	"func":       Func,
+	"for":        For,
+	"range":      Range,
 	"cappuchino": Cappuchino,
 }
 
@@ -43,48 +44,94 @@ func newToken(value string, tokenType TokenType) Token {
 	return Token{Value: value, Type: tokenType}
 }
 
-func tokenize(sourceCode string) []Token {
+func deprecatedTokenized(sourceCode string) []Token {
 	var tokens []Token
 	var currentToken strings.Builder
 	for i := 0; i < len(sourceCode); i++ {
-		char := rune(sourceCode[i]) // https://exercism.org/tracks/go/concepts/runes <-- utf32
-        if unicode.IsSpace(char) {
-            continue
-        }else if unicode.IsLetter(char){ // A letter, "LET"
-			currentToken.WriteRune(char) // "L" for example
-			for (i+1 < len(sourceCode) && unicode.IsLetter(rune(sourceCode[i+1]))) {
-                i++ 
-                currentToken.WriteRune(rune(sourceCode[i])) // "E"
-            }
-			tokenValue := currentToken.String() // LET
+		char := rune(sourceCode[i])
+		if unicode.IsLetter(char) {
+			currentToken.WriteRune(char)
+		}
+		//char := rune(sourceCode[i]) // https://exercism.org/tracks/go/concepts/runes <-- utf32
+		//if unicode.IsSpace(char) {
+		//    continue
+		//}else if unicode.IsLetter(char){ // A letter, "LET"
+		//	currentToken.WriteRune(char) // "L" for example
+		//	for (i+1 < len(sourceCode) && unicode.IsLetter(rune(sourceCode[i+1]))) {
+		//        i++
+		//        currentToken.WriteRune(rune(sourceCode[i])) // "E"
+		//    }
+		//	tokenValue := currentToken.String() // LET
+		//	tokenType, isKeyword := keywords[strings.ToLower(tokenValue)] // Is it my reserved
+		//	if !isKeyword {
+		//		tokenType = Identifier
+		//	}
+		//	tokens = append(tokens, newToken(currentToken.String(), tokenType))
+		//	currentToken.Reset()
+		//}else{
+		//	switch char {
+		//	case '-', '+', '/', '*':
+		//		tokens = append(tokens, newToken(string(char), BinaryOperator))
+		//	case '=':
+		//		tokens = append(tokens, newToken(string(char), Equals))
+		//	case '(':
+		//		tokens = append(tokens, newToken(string(char), OpenParent))
+		//	case ')':
+		//		tokens = append(tokens, newToken(string(char), CloseParent))
+		//	default:
+		//		fmt.Printf("Unexpected character: %c\n", char)
+		//	}
+		//}
+	}
+	return tokens
+}
+
+func getToken(sourceCode string) []Token {
+	var currentToken = strings.Builder{}
+	var tokens []Token
+
+	for i := 0; i < len(sourceCode); i++ {
+		thisChar := rune(sourceCode[i])
+		if unicode.IsLetter(thisChar) { //LET for example
+			currentToken.WriteRune(thisChar) // im writing L
+			// If is this a letter,  i want to check reserved
+			//like leetcode question, lets go next char before determine
+			var tempOne = i
+			for tempOne+1 < len(sourceCode) && unicode.IsLetter(rune(sourceCode[tempOne+1])) {
+				tempOne++
+				currentToken.WriteRune(rune(sourceCode[tempOne])) // im writing E
+			}
+			tokenValue := currentToken.String()
 			tokenType, isKeyword := keywords[strings.ToLower(tokenValue)] // Is it my reserved
 			if !isKeyword {
 				tokenType = Identifier
 			}
-			tokens = append(tokens, newToken(currentToken.String(), tokenType))
 			currentToken.Reset()
-		}else{
-			switch char {
-			case '-', '+', '/', '*':
-				tokens = append(tokens, newToken(string(char), BinaryOperator))
-			case '=':
-				tokens = append(tokens, newToken(string(char), Equals))
-			case '(':
-				tokens = append(tokens, newToken(string(char), OpenParent))
-			case ')':
-				tokens = append(tokens, newToken(string(char), CloseParent))
-			default:
-				fmt.Printf("Unexpected character: %c\n", char)
-			}
+			i = tempOne
+			tokens = append(tokens, Token{Value: tokenValue, Type: tokenType})
+		} else if unicode.IsDigit(thisChar) {
+			tokens = append(tokens, newToken(string(thisChar), Number))
+		} else {
+			tokens = append(tokens, newToken(string(thisChar), UnidentifiedFlyingObject))
 		}
 	}
+
 	return tokens
 }
-func main() {
-    sourceCode := "Cappuchino+-xyz" // TODO: xyz still consider
-    tokens := tokenize(sourceCode)
-    for _, token := range tokens {
-        fmt.Printf("Token Value: %s, Token Type: %d\n", token.Value, token.Type)
-    }
+
+func checkChar(thisChar rune, sourceCode string) {
+
 }
 
+func main() {
+	sourceCode := "Cappuchino+-xyz" // TODO: xyz still consider
+	tokens := getToken(sourceCode)
+
+	for _, token := range tokens {
+		fmt.Printf("Token Value: %s, Token Type: %d\n", token.Value, token.Type)
+	}
+	//tokens := tokenize(sourceCode)
+	//for _, token := range tokens {
+	//	fmt.Printf("Token Value: %s, Token Type: %d\n", token.Value, token.Type)
+	//}
+}
